5355477b4b41b94113897e1e6e0e01f91b1c5755
cafoala <catherine.russon@gmail.com>
2023-03-14 17:12:40 +0200

    paste upload

--- code/section_upload_content.py
+++ code/section_upload_content.py
@@ -12,16 +12,33 @@ def get_upload_layout():
                 dbc.Col(dbc.Alert(
                         [
                                 html.I(className="bi bi-info-circle-fill me-2"),
-                                'To begin, use the button to select the CGM files you want to work with. If you want to start again with your upload just the box again and reselect.',
+                                'To begin, use the button to select the CGM files you want to work with',
                         ],
                         color="info",
                         className="d-flex align-items-center",
-                ))]),
-
-        dbc.Row([dbc.Col(width=2),
-                dbc.Col(html.Div(du.Upload(text='Drag and drop or click to select files', 
-                                max_files=1000, id='dash-uploader', 
-                                text_completed='Upload file complete: '))),
+                ))]), 
+        dbc.Row([
+                dbc.Col(width=2),
+                dbc.Col([dcc.Upload(
+                        id='upload-data',
+                        children=html.Div([
+                        'Drag and Drop or ',
+                html.A('Select Files')
+                        ]),
+                        style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '10px'
+                        },
+                        # Allow multiple files to be uploaded
+                        multiple=True
+                ),
+                ]),
                 dbc.Col(width=2),
                 ]),
         html.Br(),
@@ -80,38 +97,25 @@ def create_file_list(list_of_names):
         data = pd.DataFrame(list_of_names)
         data.columns = ['Selected files:']
         return html.Div([
-                        html.Br(),
-                        html.Div(dash_table.DataTable(data=data.to_dict('records'),
-                                style_table={
-                                        'overflowX': 'auto',
-                                        'maxHeight': '25vh',
-                                },
-                                style_header={
-                                        'backgroundColor': 'rgb(210, 210, 210)',
-                                        'color': 'black',
-                                        'fontWeight': 'bold',
-                                        'textAlign':'left'
-                                },
-                                style_cell={
-                                        'whiteSpace': 'normal',
-                                        'font-family':'sans-serif',
-                                        'textAlign':'center'
-                                },
-                                ), style={'textAlign': 'center'}), 
-                        html.Br(),
-                        html.H6('Select the format of the date in your files:'),
-                        dbc.RadioItems(
-                                id="datetime-format",
-                                class_name="btn-group",
-                                inputClassName="btn-check",
-                                labelClassName="btn btn-outline-primary",
-                                labelCheckedClassName="active",
-                                options=[
-                                        {"label": "European", "value": 'euro'},
-                                        {"label": "American", "value": 'amer'},
-                                ],
-                                value='euro',
-                        ),
-                        html.Br(),
-                        html.P('If you\'re happy with these files, click next to process your data'), #, style={'text-align':'right'}
+            #html.H5(''),
+            html.Br(),
+            html.Div(dash_table.DataTable(data=data.to_dict('records'),
+                style_table={
+                        'overflowX': 'auto',
+                        'maxHeight': '25vh',
+                },
+                style_header={
+                        'backgroundColor': 'rgb(210, 210, 210)',
+                        'color': 'black',
+                        'fontWeight': 'bold',
+                        'textAlign':'left'
+                },
+                style_cell={
+                            'whiteSpace': 'normal',
+                            'font-family':'sans-serif',
+                            'textAlign':'center'
+                },
+                ), style={'textAlign': 'center'}), 
+            html.Br(),
+            html.P('If you\'re happy with these files, click next to process your data'), #, style={'text-align':'right'}
             ])
