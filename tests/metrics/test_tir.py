76e7fb8bace61b3d8300a96e00f0d0ede0c1c2b8
cafoala <catherine.russon@gmail.com>
2024-12-05 08:51:12 +0000

    renaming code

--- tests/metrics/test_tir.py
+++ tests/metrics/test_tir.py
@@ -6,7 +6,7 @@ import sys # added!
 #dirname = os.path.dirname(__file__)
 #filename = os.path.join(dirname, '../../code')
 #from code.metrics_helper import tir_helper
-from code.metrics_helper import tir_helper
+from webapp_code.metrics_helper import tir_helper
 
 example1 = [2.9, 3, 3.8, 3.9, 4, 9.9, 10, 10.1, 13.8, 13.9, 14]
 test_data = pd.Series(example1)
